46f772465226d9d5bae77b0c28f203c88c97ce7a
Noah Simcox <nssimcox@gmail.com>
2023-10-19 03:13:22 -0400

    Conditionals are a thing?

--- Turer.cs
+++ Turer.cs
@@ -12,15 +12,18 @@ class Turer
         int pointer = 0;
 
         Console.WriteLine("Start Coding:");
-
-        string codeline = Console.ReadLine();
-
+        
+        //Handles Readline and unbalanced parenthesis
+        char[] chararr = Console.ReadLine().ToCharArray();
+        char[] parenthesis = Array.FindAll(chararr, x => x is '(' or ')');
+        Func<int, bool> balancecheck = x => x % 2 == 0;
+        _=balancecheck(parenthesis.Length) ? "null" : throw new InvalidOperationException("The program has an unbalanced parenthesis");
 
         int count = 0;
-        while (codeline.Length > count)
+        while (chararr.Length > count)
         {
-            // codeline[count] => codeline[count] == '(' || codeline[count] == ')';
-            switch(codeline[count])
+            
+            switch(chararr[count])
             {
                 case '>':
                     tape.Add(0);
@@ -28,8 +31,7 @@ class Turer
                     break;
         
                 case '<':
-                    if (pointer == 0)
-                        throw new InvalidOperationException("The Tape cannot go below zero");
+                    _=pointer == 0 ? throw new InvalidOperationException("The Tape cannot go below zero") : "null";
                     pointer--;
                     break;
         
@@ -38,20 +40,18 @@ class Turer
                     break;
         
                 case '-':
-                    if (tape[pointer] == 0)
-                        throw new InvalidOperationException("The value of the Tape cannot go below zero");
+                    _=tape[pointer] == 0 ? throw new InvalidOperationException("The value of the Tape cannot go below zero") : "null";
                     tape[pointer]--;
                     break;
         
                 case '|':
-                    Console.Write(Convert.ToChar(tape[pointer] + 32));
+                    Console.Write(Convert.ToChar(tape[pointer]));
                     break;
         
                 case '*':
                     var input = Console.ReadLine();
-                    if (input.Length != 1) 
-                        throw new InvalidOperationException("The input must be of length 1");
-                    tape[pointer] += (int)input[0];
+                    _ = input.Length == 1 ? "null" : throw new InvalidOperationException("The input must be of length 1");
+                    tape[pointer] += input[0];
                     break;
         
                 // case '(' & pointer == 0:
